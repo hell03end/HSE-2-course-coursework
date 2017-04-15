7f5b68174e6b423553c73f618bbd2fa2a3229a24
Alex <eshobee@gmail.com>
2017-04-15 15:42:12 +0300

    renamed from backup

--- dev/ESOINN_old.py
+++ dev/ESOINN_old.py
@@ -14,7 +14,6 @@ class ESOINN_Neuron:
     def __init__(self):
         self.feature_vector = []  # weights
         self.accamulate_signals = 0
-        self.s = 0
         self.density = 0  # amount of input signals in current point
         self.mark = -1  # subclass
         self.degree = 0
@@ -40,15 +39,11 @@ class ESOINN:
         # Initial 2 neurons
         # Инициализация веторов признаков(из области допустимых значений) для двух начальных нейронов
         # Думаю, надо первые два входных вектора использовать для них
-        for i in range(2):
-            x = random.random()
-            x = round(x,4)
-            neuron1.feature_vector.append(x)
-
-        for i in range(2):
-            x = random.random()
-            x = round(x,4)
-            neuron2.feature_vector.append(x)
+        for neuron in [neuron1, neuron2]:
+            for i in range(2):
+                x = random.random()
+                x = round(x, 4)
+                neuron.feature_vector.append(x)
 
         # Инициализация матрицы смежности (2 х 2)
         # (можно переделать в словарь)
@@ -124,8 +119,7 @@ class ESOINN:
 
             if (j != self.winner2 and self.adjacency_matrix[self.winner2][j].rib_exist == 1):
                 neighbour_flag_win2 = 1
-        print(neighbour_flag_win1)
-        print(neighbour_flag_win2)
+
         # Далее, исходя из наличия соседей, ищутся параметры Та1 (для первого победителя), Та2 (для второго победителя)
         if neighbour_flag_win1 == 1:
             Ta1 = self.if_neighbour_exist(self.winner1)
@@ -145,12 +139,10 @@ class ESOINN:
         #   подать новый входной вектор, поэтому используются return 1 и return 0
         if (dist_inputVec_Win1 > Ta1):
             self.create_new_neuron(inputVec)
-            print('Created Ta1')
             return 1
 
         if (dist_inputVec_Win2 > Ta2):
             self.create_new_neuron(inputVec)
-            print('Created Ta2')
             return 1
 
         return 0
@@ -168,15 +160,23 @@ class ESOINN:
         return param_Tmax
 
     # Вычисление параметров Та1 и Та2, если смежных узлов нет
-    def if_neighbour_not_exist(self, neuron_index):
-        param_Tmax = 0
+    def if_neighbour_not_exist(self,neuron_index):
+
+        param_Tmin = 0
+
+        # Начальное значение для param_Tmin
         for j in range(self.adjacency_matrix.shape[1]):
-            dist = self.calc_dist(self.neurons[neuron_index].feature_vector, self.neurons[j].feature_vector)
-            if dist > param_Tmax:
-                param_Tmax = dist
+            if neuron_index!=j:
+                param_Tmin = self.calc_dist(self.neurons[neuron_index].feature_vector,self.neurons[j].feature_vector)
+                break
 
-        return param_Tmax
+        for j in range(self.adjacency_matrix.shape[1]):
+            if neuron_index!=j:
+                dist = self.calc_dist(self.neurons[neuron_index].feature_vector,self.neurons[j].feature_vector)
+                if dist < param_Tmin:
+                    param_Tmin = dist
 
+        return param_Tmin
 
     # Функция создания нового нейрона
     def create_new_neuron(self,inputVec):
@@ -228,7 +228,7 @@ class ESOINN:
             else:
                 # Если соединение нужно
                 if self.merge_condition_algorithm1(self.winner1,self.winner2) == 1:
-                    # self.merge_subclasses(self.winner1, self.winner2)
+                    self.merge_subclasses(self.winner1, self.winner2)
                     self.set_rib()
                 # Если нет, то старое ребро удаляется
                 else:
@@ -263,7 +263,7 @@ class ESOINN:
             param_of_B, max_dens_cluster_of_B = self.param_of_B(B)
 
             # Условия соединения двух подклассов в один (Для понимания см. в Алгоритм ESOINN)
-            if min(self.neurons[A].density,self.neurons[B].density) > param_of_A * max_dens_cluster_of_A and \
+            if min(self.neurons[A].density,self.neurons[B].density) > param_of_A * max_dens_cluster_of_A or \
                min(self.neurons[A].density,self.neurons[B].density) > param_of_B * max_dens_cluster_of_B:
 
                 return 1
@@ -339,46 +339,12 @@ class ESOINN:
             aver_dist = round(aver_dist,4)
             return aver_dist
 
-    def calc_aver_dist_in_subclass(self):
-
-        # Пошло само вычисление
-        summ_dist = 0
-        count_of_neighbours = 0
-        list_neurons_in_sb = []
-
-        for i in range(self.neurons.shape[0]):
-            if self.neurons[i].mark == self.neurons[self.winner1].mark:
-                list_neurons_in_sb.append(i)
-
-        for i in range(len(list_neurons_in_sb)):
-            for j in range(i, len(list_neurons_in_sb)):
-                if self.adjacency_matrix[i][j].rib_exist == 1:
-                    summ_dist += self.calc_dist(self.neurons[i].feature_vector, self.neurons[j].feature_vector)
-                    count_of_neighbours += 1
-
-        # Если у победителя нет соседей
-        if count_of_neighbours == 0:
-            return 0
-        # Если не один
-        else:
-            aver_dist = summ_dist / count_of_neighbours
-            aver_dist = round(aver_dist, 4)
-            return aver_dist
-
-
-
     # Вычисление плотности нейрона
     def calc_density(self):
 
         # self.neurons[self.winner1].density += 1/(((1+self.calc_aver_dist())**2))
         # Вот не понятно, какая модель лучше (ниже учитывается накопленный победителем сигнал)
-        # self.neurons[self.winner1].density += 1/(self.neurons[self.winner1].accamulate_signals*(1+self.calc_aver_dist_in_subclass())**2)
-        # self.neurons[self.winner1].density += 1/(self.neurons[self.winner1].accamulate_signals*(1+self.calc_aver_dist_in_subclass())**2)
-        # self.neurons[self.winner1].density = round(self.neurons[self.winner1].density,3)
-
-        self.neurons[self.winner1].s += 1 / ((1 + self.calc_aver_dist_in_subclass()) * (1 + self.calc_aver_dist_in_subclass()))
-        self.neurons[self.winner1].density = self.neurons[self.winner1].s
-        self.neurons[self.winner1].density = round(self.neurons[self.winner1].density, 3)
+        self.neurons[self.winner1].density += 1/(self.neurons[self.winner1].accamulate_signals*((1+self.calc_aver_dist())**2))
 
     # Смещение вектора признака нейронов победителей
     def update_feature_vector(self,inputVec):
@@ -438,7 +404,7 @@ class ESOINN:
 
         list_of_apexes = []
         for i in range(self.neurons.shape[0]):
-            self.neurons[i].mark = -1
+            self.neurons[i].mark = -2
 
         # Находим вершины подклассов
         for i in range(self.adjacency_matrix.shape[0]):
@@ -446,10 +412,11 @@ class ESOINN:
             max_dens_arround = self.neurons[i].density
             max_dens_index = i
 
+
             # Тут глупый быдло-код, по нахождении самих вершин
             for j in range(self.adjacency_matrix.shape[1]):
                 if self.adjacency_matrix[i][j].rib_exist == 1:
-                    if self.neurons[j].density > max_dens_arround and j!=i:
+                    if self.neurons[j].density > max_dens_arround:
                         max_dens_index = j
                         break
 
@@ -465,20 +432,12 @@ class ESOINN:
                         if self.neurons[i].density > self.neurons[list_of_apexes[j]].density:
                             list_of_apexes.insert(j,i)
                             break
-                    # Если за цикл ни разу не зашел в if
-                    list_of_apexes.append(i)
-
-        # if self.quantity_of_inputVec % 100 == 0:
-        #     self.build_graph_type1(2)
 
         print('list of apexes',list_of_apexes)
 
         # Для каждой вершины маркировать соседние вершины, если их плотность меньше (такой же маркировкой, что и верщина)
         for i in list_of_apexes:
             self.mark_adjacent_neurons(i,self.neurons[i].mark)
-
-        # if self.quantity_of_inputVec % 100 == 0:
-        #     self.build_graph_type1(2)
 
         # Разделение разных подклассов или их объединение
         self.partition_class()
@@ -488,7 +447,7 @@ class ESOINN:
         for j in range(self.adjacency_matrix.shape[1]):
             if self.adjacency_matrix[indexB][j].rib_exist == 1 and \
             self.neurons[j].density <= self.neurons[indexB].density and \
-            self.neurons[j].mark == -1:
+            self.neurons[j].mark == -2:
                 self.neurons[j].mark = mark
                 self.mark_adjacent_neurons(j,mark)
 
@@ -496,29 +455,27 @@ class ESOINN:
         for i in range(self.adjacency_matrix.shape[0]):
             for j in range(i+1, self.adjacency_matrix.shape[1]):
                 if self.adjacency_matrix[i][j].rib_exist == 1:
-                    if self.neurons[i].mark != self.neurons[j].mark and self.neurons[j].mark != -1 and \
-                    self.neurons[i].mark != -1:
+                    if self.neurons[i].mark != self.neurons[j].mark:
                         # Объединение подклассов
                         if self.merge_condition_algorithm1(i,j) == 1:
                             self.merge_subclasses(i,j)
                         # Разъединение узлов
-                        else:
-                            self.adjacency_matrix[i][j].rib_exist = 0
-                            self.adjacency_matrix[j][i].rib_exist = 0
+                        # else:
+                        #     self.adjacency_matrix[i][j].rib_exist = 0
+                        #     self.adjacency_matrix[j][i].rib_exist = 0
+                        #
+                        #     self.adjacency_matrix[i][j].rib_age = 0
+                        #     self.adjacency_matrix[j][i].rib_age = 0
 
-                            self.adjacency_matrix[i][j].rib_age = 0
-                            self.adjacency_matrix[j][i].rib_age = 0
+        for i in range(self.adjacency_matrix.shape[0]):
+            for j in range(i+1, self.adjacency_matrix.shape[1]):
+                if self.adjacency_matrix[i][j].rib_exist == 1:
+                    if self.neurons[i].mark != self.neurons[j].mark:
+                        self.adjacency_matrix[i][j].rib_exist = 0
+                        self.adjacency_matrix[j][i].rib_exist = 0
 
-        # Разъединение узлов
-        # for i in range(self.adjacency_matrix.shape[0]):
-        #     for j in range(i+1, self.adjacency_matrix.shape[1]):
-        #         if self.adjacency_matrix[i][j].rib_exist == 1:
-        #             if self.neurons[i].mark != self.neurons[j].mark:
-        #                 self.adjacency_matrix[i][j].rib_exist = 0
-        #                 self.adjacency_matrix[j][i].rib_exist = 0
-        #
-        #                 self.adjacency_matrix[i][j].rib_age = 0
-        #                 self.adjacency_matrix[j][i].rib_age = 0
+                        self.adjacency_matrix[i][j].rib_age = 0
+                        self.adjacency_matrix[j][i].rib_age = 0
 
         #  Удаление шумов
         self.remove_noise()
@@ -536,21 +493,16 @@ class ESOINN:
     # Удаление шумов
     def remove_noise(self):
         # Поиск нейронов - шумов
-        flag = 0
         index_of_noise = self.search_noise()
-        print(index_of_noise)
         index_of_noise = list(reversed(index_of_noise))
         if index_of_noise != None:
             for i in index_of_noise:
-                flag = 1
                 self.quantity_of_delete_neurons += 1
 
                 self.adjacency_matrix = np.delete(self.adjacency_matrix, i, 0)
                 self.adjacency_matrix = np.delete(self.adjacency_matrix, i, 1)
 
                 self.neurons = np.delete(self.neurons, i, 0)
-        # if flag == 1:
-        #     self.remove_noise()
 
     # Поиск нейронов - шумов
     def search_noise(self):
@@ -592,15 +544,12 @@ class ESOINN:
 
     def inputX(self, inputVec):
 
-
-        self.winner1, self.winner2 = self.find_winners(inputVec)
-        # if self.quantity_of_inputVec % 100 == 0:
-        #     self.build_graph_type1(inputVec, 1)
-        if self.point4(inputVec) == 1:
-            # if self.quantity_of_inputVec % self.lambd == 0 and self.quantity_of_inputVec != 0:
-            #     self.mark_neurons()
-            return 0
         self.quantity_of_inputVec += 1
+        self.winner1, self.winner2 = self.find_winners(inputVec)
+        if self.point4(inputVec) == 1:
+            if self.quantity_of_inputVec % self.lambd == 0 and self.quantity_of_inputVec != 0:
+                self.mark_neurons()
+            return 0
         self.update_ribs_age()
         self.rib_between_win1_win2()
         self.calc_density()
@@ -608,10 +557,6 @@ class ESOINN:
         self.threshold_check()
         if self.quantity_of_inputVec % self.lambd == 0 and self.quantity_of_inputVec != 0:
             self.mark_neurons()
-            print('1')
-        if self.quantity_of_inputVec % 100 == 0:
-            print('2')
-            self.build_graph_type1(2)
 
     # Рекурсивный обход по компоненту связности
     def recur_matrix(self,i, neurons_in_one_component):
@@ -645,39 +590,8 @@ class ESOINN:
         return neurons_in_components
 
 
-    def build_graph_type1(self, param):
-        plt.ion()
-        plt.axis([0,1,0,1])
-        # Потроение нейронов и вывод их маркировки
-        for i in range(self.neurons.shape[0]):
-            plt.plot(self.neurons[i].feature_vector[0],self.neurons[i].feature_vector[1], 'ro', color = 'red')
-            plt.text(self.neurons[i].feature_vector[0], self.neurons[i].feature_vector[1] + 0.002,
-                     self.neurons[i].mark, fontsize=8)
-            plt.text(self.neurons[i].feature_vector[0], self.neurons[i].feature_vector[1] - 0.02,
-                     self.neurons[i].density, fontsize=8)
-            if param == 1:
-                plt.plot(self.neurons[self.winner1].feature_vector[0], self.neurons[self.winner1].feature_vector[1],
-                         'ro', color = 'green')
-                plt.plot(self.neurons[self.winner2].feature_vector[0], self.neurons[self.winner2].feature_vector[1],
-                         'ro', color = 'blue')
-
-        for i in range(self.adjacency_matrix.shape[0]):
-            for j in range(i+1, self.adjacency_matrix.shape[1]):
-                if self.adjacency_matrix[i][j].rib_exist == 1:
-                    plt.plot([self.neurons[i].feature_vector[0], self.neurons[j].feature_vector[0]],
-                             [self.neurons[i].feature_vector[1], self.neurons[j].feature_vector[1]],
-                             color='red', hold='false')
-
-        plt.legend('6')
-        plt.pause(0.001)
-        plt.clf()
-
-
-
-
-
 # Создание обхекта ESOINN
-ExampleStart = ESOINN(50, 0.001, 1, 100)
+ExampleStart = ESOINN(50, 0.001, 1, 50)
 
 # Списки координат для тестов
 all_test = []
@@ -685,24 +599,15 @@ all_test_x = []
 all_test_y = []
 
 # Генерация чисел, надеюсь разберетсь (Гаусс1)
-for i in range(10000):
+for i in range(5000):
     sample = []
-
-    # noise
-    if i % 10 == 0:
-        sample.append(random.random())
-    # correct data
-    else:
-        sample.append(random.gauss(0.7,0.15))
-
+    sample.append(random.gauss(0.7,0.15))
+    # sample.append(random.random())
     sample[0] = round(sample[0],4)
     all_test_x.append(sample[0])
 
-    if i % 10 == 0:
-        sample.append(random.random())
-    else:
-        sample.append(random.gauss(0.7,0.15))
-
+    sample.append(random.gauss(0.7,0.15))
+    # sample.append(random.random())
     all_test_y.append(sample[1])
     sample[1] = round(sample[1],4)
     all_test.append(sample)
@@ -712,24 +617,15 @@ for i in range(10000):
     ExampleStart.inputX(sample)
 
 # (Гаусс2)
-for i in range(10000):
+for i in range(5000):
     sample = []
-
-    # noise
-    if i % 10 == 0:
-        sample.append(random.random())
-    # correct data
-    else :
-        sample.append(random.gauss(0.3,0.15))
-
+    sample.append(random.gauss(0.3,0.15))
+    # sample.append(random.random())
     sample[0] = round(sample[0],4)
     all_test_x.append(sample[0])
 
-    if i % 10 == 0:
-        sample.append(random.random())
-    else:
-        sample.append(random.gauss(0.3,0.15))
-
+    sample.append(random.gauss(0.3,0.15))
+    # sample.append(random.random())
     all_test_y.append(sample[1])
     sample[1] = round(sample[1],4)
     all_test.append(sample)
@@ -737,6 +633,20 @@ for i in range(10000):
     print('sample = ', i)
     ExampleStart.inputX(sample)
 
+# Шум 10%
+# for i in range(1001):
+#     sample = []
+#     sample.append(random.random())
+#     sample[0] = round(sample[0],4)
+#     all_test_x.append(sample[0])
+#
+#     sample.append(random.random())
+#     all_test_y.append(sample[1])
+#     sample[1] = round(sample[1],4)
+#     all_test.append(sample)
+#
+#     print('sample = ', i)
+#     ExampleStart.inputX(sample)
 
 # Вывод конечной матрицы
 for i in range(ExampleStart.adjacency_matrix.shape[0]):
@@ -744,6 +654,23 @@ for i in range(ExampleStart.adjacency_matrix.shape[0]):
         print (ExampleStart.adjacency_matrix[i][j].rib_exist, end=' ')
     print()
 
+
+
+# for i in range(5000):
+#     sample = []
+#     sample.append(random.gauss(0.5,0.1))
+#     # sample.append(random.random())
+#     sample[0] = round(sample[0],4)
+#     all_test_x.append(sample[0])
+#
+#     sample.append(random.gauss(0.5,0.1))
+#     # sample.append(random.random())
+#     all_test_y.append(sample[1])
+#     sample[1] = round(sample[1],4)
+#     all_test.append(sample)
+#
+#     print('sample = ', i)
+#     ExampleStart.inputX(sample)
 
 # Вывод списка нейронов
 for i in range(ExampleStart.neurons.shape[0]):
@@ -768,34 +695,25 @@ for i in range(len(neurons_in_comp)):
     print('component = ',i,': ')
     print(neurons_in_comp[i])
 
-plt.ioff()
-
 # Построение нейронов
 plt.plot([x], [y], 'ro',hold='false')
 plt.plot([x], [y], 'ro',hold='false')
-
-# Вывод их маркировки
 for i in range(ExampleStart.neurons.shape[0]):
     plt.text(x[i],y[i]+0.002, ExampleStart.neurons[i].mark, fontsize=8)
 
-for i in range(ExampleStart.neurons.shape[0]):
-    plt.text(ExampleStart.neurons[i].feature_vector[0], ExampleStart.neurons[i].feature_vector[1] - 0.02,
-             ExampleStart.neurons[i].density, fontsize=8)
-
-# Отображение ребер
 for i in range(ExampleStart.adjacency_matrix.shape[0]):
     for j in range(i+1, ExampleStart.adjacency_matrix.shape[1]):
         if ExampleStart.adjacency_matrix[i][j].rib_exist == 1:
             plt.plot([ExampleStart.neurons[i].feature_vector[0],ExampleStart.neurons[j].feature_vector[0]],
             [ExampleStart.neurons[i].feature_vector[1], ExampleStart.neurons[j].feature_vector[1]], color = 'red',hold='false')
 
-for i in range(ExampleStart.neurons.shape[0]):
-    if ExampleStart.neurons[i].density > 4:
-        print(ExampleStart.neurons[i].accamulate_signals)
-
 # Построение тестовой выборки
 # plt.plot([all_test_x],[all_test_y],'bo')
 plt.axis([0,1,0,1])
-plt.legend('6')
 plt.show()
 
+print("ПАЕШТ")
+#
+# for i in range(2):
+#     for j in range(3):
+#         print(ExampleStart.R[i][j].rib_age)
